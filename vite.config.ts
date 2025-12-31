b27492f989716ef574139fbb04531729dbf9d647
蔡鑫雯 <1187695357@qq.com>
2025-12-31 16:50:46 +0800

    feat(build): 添加rollup打包分析工具并优化分包策略
    添加rollup-plugin-visualizer插件用于分析打包体积
    配置vite的build.rollupOptions实现node_modules分包

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,32 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({
+      open: true,
+    }),
+  ],
   resolve: {
     alias: {
-      "@": "/src",
+      "@": resolve(__dirname, "src"),
+    },
+  },
+  // 分包
+  build: {
+    rollupOptions: {
+      output: {
+        // dir: resolve(__dirname, "todo"),
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            // console.log(id.toString().split("node_modules/")[1].split("/")[1]);
+            return id.toString().split("node_modules/")[1].split("/")[1];
+          }
+        },
+      },
     },
   },
 });
